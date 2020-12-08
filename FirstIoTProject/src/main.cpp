51c3078d201bad6d9a78c7c6b2e9d4d4c459e74c
Sven Pohl <cloudgeek@hotmail.com>
2020-12-08 11:42:58 +0100

    clean up

---
+++ FirstIoTProject/src/main.cpp
@@ -0,0 +1,46 @@
+/*
+    This sketch establishes a TCP connection to a "quote of the day" service.
+    It sends a "hello" message, and then prints received data.
+*/
+
+#include <ESP8266WiFi.h>
+
+#ifndef STASSID
+#define STASSID "wlan.team-pohl.rocks"
+#define STAPSK  "Y3M9GKiDx9RLTnx3kqN5"
+#endif
+
+const char* ssid     = STASSID;
+const char* password = STAPSK;
+
+void initWifi(){
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  initWifi();
+}
+
+void loop() {
+
+}
